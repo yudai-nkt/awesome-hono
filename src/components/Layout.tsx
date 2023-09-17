30982a8b8db5f075a1e90a9d9214284e4978f96b
Yudai NAKATA <yudai.nkt@gmail.com>
2023-09-17 13:32:54 +0900

    refactor: tweak Layout definition

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,13 +12,13 @@ export const Layout = ({
   description: string;
   children: HtmlEscapedString;
 }) =>
-  html`<!DOCTYPE html>
+  prependDoctype(
     <html lang="en">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width" />
-        <meta name="description" content="${description}" />
-        <title>${title}</title>
+        <meta name="description" content={description} />
+        <title>{title}</title>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/picocss/2.0.0-alpha1/pico.min.css"
@@ -27,10 +27,10 @@ export const Layout = ({
           referrerpolicy="no-referrer"
         />
         <link rel="stylesheet" href="/static/custom.css" />
-        <meta property="og:description" content="${description}" />
+        <meta property="og:description" content={description} />
         <meta property="og:image" content="/static/ogp-image.png" />
         <meta property="og:site_name" content="Awesome Hono" />
-        <meta property="og:title" content="${title}" />
+        <meta property="og:title" content={title} />
       </head>
       <body>
         <header class="container">
@@ -38,11 +38,13 @@ export const Layout = ({
             <h1>Awesome Hono</h1>
             <p>A curated list of awesome stuff related to Hono</p>
           </hgroup>
-          ${(<GitHubCorner />)}
+          <GitHubCorner />
         </header>
-        <main class="container">${children}</main>
+        <main class="container">{children}</main>
         <footer class="container">
           Built with Hono 🔥, curated by the community 👨‍👩‍👧‍👦
         </footer>
       </body>
-    </html>`;
+    </html>
+  );
+const prependDoctype = (jsx: HtmlEscapedString) => html`<!DOCTYPE html>${jsx}`;
